82260a3b00aaad41a49aa370beecb30d4e4d7fda
raul-blackjack <jimenezverduraul@iesizpisuabelmonte.es>
2026-09-15 10:21:36 +0200

    un nuevo archivo
    esta mu guapo

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Manu";
+$city = "Paris";
+$age = 25;
+$height = 1.75;
+
+echo "Nombre: $name" .PHP_EOL;
+echo "Ciudad: $city" .PHP_EOL;
+echo "Edad: $age" .PHP_EOL;
+echo "Altura: $height" .PHP_EOL;
+
+
